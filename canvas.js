85035238b9b7bcb755cc23575a34cf31b0f04407
JordanCollier <jordanicollier@gmail.com>
2015-05-06 11:23:31 -0600

    make a function that generates random circle and squares

--- canvas.js
+++ canvas.js
@@ -1,6 +1,39 @@
-// function drawBorder(){
-//     var canvas = document.getElementById("canvas");
-//     var context = canvas.getContext('2d');
-//     var size = { x: canvas.width, y: canvas.height }
-//     context.strokeRect(0,0, size.x, size.y);
-// }
+
+  var canvas = document.getElementById("canvas");
+  var context = canvas.getContext('2d');
+  var size = { x: canvas.width, y: canvas.height }
+  var radius = Math.floor(Math.random() * 250) + 1;
+
+  function drawBorder(){
+      context.strokeRect(0,0, size.x, size.y);
+  }
+
+  function fillBlue() {
+    context.fillStyle = "blue"
+    context.fillRect(0, 0, size.x/2, size.y/2)
+  }
+
+  function fillRed() {
+    context.fillStyle = "red"
+    context.fillRect(size.x/2, size.y/2, size.x/2, size.y/2)
+  }
+
+  function fillRand(color) {
+  var random = Math.floor((Math.random() * 2) + 1);
+  context.fillStyle = color;
+  if (random === 1) {
+    context.fillRect(Math.floor((Math.random() * (size.x-200)) + 1), Math.floor((Math.random() * (size.y-200)) + 1), Math.floor((Math.random() * (size.x-200)) + 1), Math.floor((Math.random() * (size.y-200)) + 1));
+  }
+  else {
+    context.arc(Math.floor((Math.random() * (size.x-200)) + 1), Math.floor((Math.random() * (size.y-200)) + 1), Math.floor((Math.random() * (size.x-200)) + 1), 0, 2 * Math.PI);
+    context.fill();
+    context.closePath();
+  }
+}
+
+  function clearCanvas() {
+    context.clearRect ( 1 , 1 , size.x -2, size.y -2 );
+
+  }
+
+drawBorder();
